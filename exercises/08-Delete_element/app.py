c49b60d1597748d1be79a64f44f990506f4021b3
Sergio Roque Hernández <sergioroquehernandez93@gmail.com>
2025-02-21 20:30:30 +0000

    Finalizados

--- exercises/08-Delete_element/app.py
+++ exercises/08-Delete_element/app.py
@@ -2,7 +2,7 @@ people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 
 def delete_person(person_name):
     # Your code here
-    
+    return [name for name in people if name != person_name]
 
     
 # Don't delete anything below
